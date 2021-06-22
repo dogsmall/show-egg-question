2d358f19bdcfa94e72c860a9674d69af46005993
guoshiwei <guoshiwei@tal.com>
2021-06-22 20:25:29 +0800

    first commit

---
+++ config/config.default.js
@@ -0,0 +1,36 @@
+/* eslint valid-jsdoc: "off" */
+
+'use strict';
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = exports = {};
+  config.cluster = {
+    listen: {
+      port: 7001,
+      hostname: '0.0.0.0', // 不建议设置 hostname 为 '0.0.0.0'，它将允许来自外部网络和来源的连接，请在知晓风险的情况下使用
+      // path: '/var/run/egg.sock',
+    },
+  };
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1624361576722_1465';
+
+  // add your middleware config here
+  config.middleware = [];
+
+  // add your user config here
+  const userConfig = {
+    // myAppName: 'egg',
+  };
+
+  return {
+    ...config,
+    ...userConfig,
+  };
+};
